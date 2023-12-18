4b2ed7ace41ccf6a9cec7af1706c6ff28bdc1dfc
Martynas Petuška <martynas@petuska.dev>
2023-12-18 01:14:34 +0000

    Button props and signals

--- gtk-compose/gtk-compose-foundation/src/nativeMain/kotlin/dev/petuska/gtk/compose/foundation/Button.kt
+++ gtk-compose/gtk-compose-foundation/src/nativeMain/kotlin/dev/petuska/gtk/compose/foundation/Button.kt
@@ -5,9 +5,7 @@ import dev.petuska.gtk.compose.ui.internal.GtkComposeInternalApi
 import dev.petuska.gtk.compose.ui.node.ContentBuilder
 import dev.petuska.gtk.compose.ui.node.GtkNode
 import dev.petuska.gtk.compose.ui.node.GtkParentNode
-import dev.petuska.gtk.compose.ui.props.PropsBuilder
-import dev.petuska.gtk.compose.ui.props.PropsScope
-import dev.petuska.gtk.compose.ui.props.signal
+import dev.petuska.gtk.compose.ui.props.*
 import org.gtkkn.bindings.gtk.Button
 import org.gtkkn.bindings.gtk.Widget
 
@@ -21,6 +19,46 @@ private class ButtonNode(override val widget: Button) : GtkParentNode<Button>() 
         widget.child = null
     }
 }
+
+// region properties
+/**
+ * @see Button.canShrink
+ */
+public var PropsScope<out Button>.canShrink: Boolean by Button::canShrink
+
+/**
+ * @see Button.child
+ */
+public var PropsScope<out Button>.child: Widget? by Button::child
+
+/**
+ * @see Button.hasFrame
+ */
+public var PropsScope<out Button>.hasFrame: Boolean by Button::hasFrame
+
+/**
+ * @see Button.getIconName
+ * @see Button.setIconName
+ */
+public var PropsScope<out Button>.iconName: String by SyntheticProperty(
+    get = { getIconName() ?: "" },
+    set = Button::setIconName,
+)
+
+/**
+ * @see Button.getLabel
+ * @see Button.setLabel
+ */
+public var PropsScope<out Button>.label: String by SyntheticProperty(
+    get = { getLabel() ?: "" },
+    set = Button::setLabel,
+)
+
+/**
+ * @see Button.useUnderline
+ */
+public var PropsScope<out Button>.useUnderline: Boolean by Button::useUnderline
+//endregion
 
 // region signals
 /**
